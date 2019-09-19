a15a6a7fce83a3429be96ea75f12470a5f0dc22b
Kenta Hayakawa <pentankentan@yahoo.com>
2019-09-19 16:20:28 -0700

    update base

--- BaseStepper/BaseStepper.ino
+++ BaseStepper/BaseStepper.ino
@@ -1,43 +1,46 @@
 
 /*
- Stepper Motor Control - one revolution
+ Stepper Motor Control - speed control
 
  This program drives a unipolar or bipolar stepper motor.
  The motor is attached to digital pins 8 - 11 of the Arduino.
+ A potentiometer is connected to analog input 0.
 
- The motor should revolve one revolution in one direction, then
- one revolution in the other direction.
+ The motor will rotate in a clockwise direction. The higher the potentiometer value,
+ the faster the motor speed. Because setSpeed() sets the delay between steps,
+ you may notice the motor is less responsive to changes in the sensor value at
+ low speeds.
 
-
- Created 11 Mar. 2007
- Modified 30 Nov. 2009
+ Created 30 Nov. 2009
+ Modified 28 Oct 2010
  by Tom Igoe
 
  */
 
 #include <Stepper.h>
 
-const int stepsPerRevolution = 0;  // change this to fit the number of steps per revolution
+const int stepsPerRevolution = 300;  // change this to fit the number of steps per revolution
 // for your motor
+
 
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
 
+int stepCount = 0;  // number of steps the motor has taken
+
 void setup() {
-  // set the speed at 60 rpm:
-  myStepper.setSpeed(60);
-  // initialize the serial port:
-  Serial.begin(9600);
+  // nothing to do inside the setup
 }
 
 void loop() {
-  // step one revolution  in one direction:
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(500);
-
-  // step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(500);
+  // read the sensor value:
+  int sensorReading = analogRead(A0);
+  // map it to a range from 0 to 100:
+  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+  // set the motor speed:
+  if (motorSpeed > 0) {
+    myStepper.setSpeed(motorSpeed);
+    // step 1/100 of a revolution:
+    myStepper.step(stepsPerRevolution / 100);
+  }
 }
